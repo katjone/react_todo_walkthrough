f7fb717e13fb786e3df34da3e5714b68b7effe43
justincastilla <pxlperfection@gmail.com>
2018-09-19 13:27:36 -0700

    updating instructions, styles

---
+++ solution/todo/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Header extends Component{
+  render(){
+    return (
+      <header>
+        <h1>ToDo</h1>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/todos'}>Todos</Link>
+      </header>
+    )
+  }
+}
+
+export default Header
---
+++ solution/todo/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+class Home extends Component{
+  render() {
+    return (
+      <div>
+        I am a Home component  
+      </div>
+    )
+  }
+
+}
+
+export default Home;
